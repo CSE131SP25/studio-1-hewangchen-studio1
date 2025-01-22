49331e844a9d7f523ef2d4e0bc92ddef2c61a7a4
richardhe <richardhe@localhost>
2025-01-22 14:07:26 -0600

    hi

---
+++ src/studio1/Average.java
@@ -0,0 +1,15 @@
+package studio1;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter value for avg: ");
+		int s0 = in.nextint();
+		
+		System.out.println("The first of two integers to be averaged? " + s0+s1 + );
+
+
+	}
+
+}
